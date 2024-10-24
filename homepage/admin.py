e943e844b7b735d8f749b4e876f94f711793d3d1
HamletJr <jogosboy1902@gmail.com>
2024-10-24 22:32:03 +0700

    tambah kolom untuk view admin

--- homepage/admin.py
+++ homepage/admin.py
@@ -3,6 +3,15 @@ from .models import Likes
 from main.models import Food, Restaurant
 
 # Register your models here.
-admin.site.register(Food)
-admin.site.register(Restaurant)
-admin.site.register(Likes)
+class FoodAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nama', 'kategori', 'harga', 'diskon', 'deskripsi')
+
+class RestaurantAdmin(admin.ModelAdmin):
+    list_display = ('id', 'nama', 'kategori', 'deskripsi')
+
+class LikesAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'food_id')
+
+admin.site.register(Food, FoodAdmin)
+admin.site.register(Restaurant, RestaurantAdmin)
+admin.site.register(Likes, LikesAdmin)

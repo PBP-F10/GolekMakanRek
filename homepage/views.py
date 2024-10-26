5078dcd0702a31e93af4ef86e0fc8990de175b97
HamletJr <jogosboy1902@gmail.com>
2024-10-27 02:47:08 +0700

    perbaiki views dan template

--- homepage/views.py
+++ homepage/views.py
@@ -6,10 +6,12 @@ from .models import Likes
 from django.http import HttpResponse, HttpResponseRedirect
 from django.core import serializers
 from django.urls import reverse
+from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.csrf import csrf_exempt
 from django.views.decorators.http import require_POST
 from django.db.models import F, ExpressionWrapper, IntegerField
+import datetime
 import os
 import json
 

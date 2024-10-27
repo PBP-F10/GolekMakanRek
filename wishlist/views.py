4cd87fcf7ff9c27e19ea44482a5cb6da59688433
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-10-27 11:36:58 +0700

    coba navbar

--- wishlist/views.py
+++ wishlist/views.py
@@ -3,24 +3,19 @@ from django.urls import reverse
 from django.http import HttpResponseRedirect
 from wishlist.models import Wishlist
 import json
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+@login_required
 def show_wishlist(request):
     wishlist = Wishlist.objects.filter(user=request.user)
     context = {
         "wishlist": wishlist
     }
 
-    ## Ini untuk ngetes ##
-    # f = open('gofood_dataset.json')
-    # p = json.load(f)
-    # f.close()
-    # context = {
-    #     "wishlist": p
-    # }
-
     return render(request, 'wishlist.html', context)
 
+@login_required
 def delete_item(request, id):
     item = Wishlist.objects.get(pk=id)
     item.delete()

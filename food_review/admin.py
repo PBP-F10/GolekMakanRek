9d221b93e895131baa3caa5450ef5d1850f4f3e3
Muhammad Afwan Hafizh <mirananightfall228@gmail.com>
2024-10-28 05:34:12 +0700

    add comment

--- food_review/admin.py
+++ food_review/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from .models import *
 
+# gatau knp tiba2 bisa keapus t_t
 class RatingComment(admin.ModelAdmin):
     list_display = ('id', 'user', 'deskripsi_food', 'score', 'comment', 'waktu_comment')
 

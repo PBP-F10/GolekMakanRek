59ce39fa8efb151243043dc41e9b1bcaca5a82a1
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-10-27 10:57:17 +0700

    buat navbar more responsive

--- userprofile/urls.py
+++ userprofile/urls.py
@@ -4,7 +4,7 @@ from userprofile.views import *
 app_name = 'userprofile'
 
 urlpatterns = [
-    path('userprofile', show_user_profile_page, name='userprofile'),    
-    path('userprofiel/update', update_user_profile, name="update_user_profile"),
-    path('userprofile/get', get_user_profile, name="get_user_profile")
+    path('', show_user_profile_page, name='userprofile'),    
+    path('update/', update_user_profile, name="update_user_profile"),
+    path('get/', get_user_profile, name="get_user_profile")
 ]

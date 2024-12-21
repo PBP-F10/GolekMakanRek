df712842b26a8c63e251322aae1f35037c1e499b
nsrnannaisha <nisrinaannaisha@gmail.com>
2024-12-21 11:56:57 +0700

    fix integrasi resto

--- resto_preview/urls.py
+++ resto_preview/urls.py
@@ -9,5 +9,12 @@ urlpatterns = [
     path('<uuid:restaurant_id>/submit-rating/', views.submit_rating, name='submit_rating'),
     path('follow/<uuid:restaurant_id>/', views.follow_restaurant, name='follow_restaurant'), 
     path('unfollow/<uuid:restaurant_id>/', views.unfollow_restaurant, name='unfollow_restaurant'), 
+
+    path('restorating_json/', views.restorating_json, name='resto_rating'),
+    path('get-restaurant-rating/<uuid:restaurant_id>/', views.get_restaurant_rating, name='get_restaurant_rating'),
+    path('get-user-rating/<uuid:restaurant_id>/', views.get_user_rating, name='get_user_rating'),
+
+    path('restofollow_json/', views.restofollow_json, name='resto_follow'),
+    path('status/', views.get_follow_status, name='get_follow_status'),
 ]
 
--- resto_preview/views.py
+++ resto_preview/views.py
@@ -1,8 +1,10 @@
 from .models import Restaurant, Food, Rating, Follow
 from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Avg
-from django.http import JsonResponse
+from django.http import JsonResponse, HttpResponse
 from django.contrib.auth.decorators import login_required
+from django.views.decorators.csrf import csrf_exempt
+from django.core import serializers
 
 def restaurant_preview(request):
     restaurants = Restaurant.objects.all()
@@ -24,6 +26,7 @@ def restaurant_detail(request, restaurant_id):
 
     total_ratings = Rating.objects.filter(restaurant=restaurant).count() 
 
+    # print("user " + str(request.user.id))
     return render(request, 'restaurant_detail.html', {
         'restaurant': restaurant,
         'foods': restaurant_foods,
@@ -32,6 +35,7 @@ def restaurant_detail(request, restaurant_id):
         'total_ratings': total_ratings,
     })
 
+@csrf_exempt
 @login_required
 def submit_rating(request, restaurant_id):  
     if request.method == 'POST':
@@ -54,31 +58,70 @@ def submit_rating(request, restaurant_id):
 
     return JsonResponse({'error': 'Invalid request'}, status=400)
 
+@csrf_exempt
 @login_required
 def follow_restaurant(request, restaurant_id):
     if request.method == "POST":
-        if request.user.is_authenticated:
-            restaurant = get_object_or_404(Restaurant, id=restaurant_id)
-            follow = Follow.objects.get_or_create(user=request.user, restaurant=restaurant)
-            if not follow:
-                follow.delete()
-            return redirect('resto_preview:restaurant_detail', restaurant_id=restaurant_id)
-        else:
-            return redirect('resto_preview:show_preview', alert="Silakan login untuk mengikuti restoran.")
-    return redirect('resto_preview:show_preview')
+        restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+        follow = Follow.objects.get_or_create(user=request.user, restaurant=restaurant)
+        if not follow:
+            follow.delete()
+    return redirect('resto_preview:restaurant_detail', restaurant_id=restaurant_id)
 
+@csrf_exempt
 @login_required
 def unfollow_restaurant(request, restaurant_id):
     if request.method == "POST":
-        if request.user.is_authenticated:
-            restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+        restaurant = get_object_or_404(Restaurant, id=restaurant_id)
 
-            try:
-                follow = Follow.objects.get(user=request.user, restaurant=restaurant)
-                follow.delete() 
-            except Follow.DoesNotExist:
-                pass
+        follow = Follow.objects.get(user=request.user, restaurant=restaurant)
+        follow.delete() 
 
-            return redirect('resto_preview:restaurant_detail', restaurant_id=restaurant_id)
+    return redirect('resto_preview:restaurant_detail', restaurant_id=restaurant_id)
 
-    return redirect('resto_preview:restaurant_detail')
+def get_restaurant_rating(request, restaurant_id):
+    restaurant = get_object_or_404(Restaurant, id=restaurant_id)
+    average_rating = restaurant.rating_set.aggregate(Avg('score'))['score__avg']
+    total_ratings = Rating.objects.filter(restaurant=restaurant).count() 
+
+    return JsonResponse({
+        'has_rating': True,
+        'restaurant_id': restaurant.id,
+        'average_rating': average_rating if average_rating else 'No ratings yet',
+        'total_ratings': total_ratings,
+    })
+
+    return serialize_data(request, Rating, "json")
+
+def get_user_rating(request, restaurant_id):
+    user_rating = Rating.objects.filter(user=request.user, restaurant=restaurant_id).first()
+    
+    return JsonResponse({
+        'has_rating': True,
+        'restaurant_id': restaurant_id,
+        'user_rating': user_rating.score,
+    })
+
+    return serialize_data(request, Rating, "json")
+
+def get_follow_status(request):
+    list_follow = list(Follow.objects.filter(user=request.user).values_list('restaurant_id', flat=True))
+
+    return JsonResponse({
+        'has_follow': True,
+        'followed_restaurant': list_follow,
+    })
+    
+def serialize_data(request, model, fmt, id=None):
+    if id:
+        data = get_object_or_404(model, pk=id)
+        data = [data]
+    else:
+        data = model.objects.all()
+    return HttpResponse(serializers.serialize(fmt, data), content_type=f"application/{fmt}")
+
+def restorating_json(request):
+    return serialize_data(request, Rating, "json")
+
+def restofollow_json(request):
+    return serialize_data(request, Follow, "json")
